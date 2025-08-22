fd019c245e90a48774b6192304ee4fbe6b65d003
vikki123224v <vighneshwaras283@gmail.com>
2025-08-22 16:08:18 +0530

    Create Leaky.java

---
+++ Leaky.java
@@ -0,0 +1,52 @@
+public static void main(String[] args) {
+    int i;
+    int a[]=new int [20];
+    int buck_rem=0,sent,recv;
+    
+    Scanner in=new Scanner(System.in);
+    System.out.println("Enter the number of Packets:");
+    int n=in.nextInt();
+    System.out.println("ENter the bucket capacity:");
+    int buck_cap=in.nextInt();
+    
+    System.out.println("Enter the output rate:");
+    int rate=in.nextInt();
+    
+    System.out.println("Enter the size of packets");
+    for(i=1;i<=n;i++)
+        a[i]=in.nextInt();
+    System.out.println("CLOCK\t PACKET SIZE \t ACCEPT \t SENT \t REMAINNIG");
+    for(i=1;i<=n;i++)
+    {
+        if(a[i]!=0)
+        {
+            if(buck_rem+a[i]>buck_cap)
+                recv=-1;
+            else{
+                recv=a[i];
+                buck_rem+=a[i];
+            }
+        }
+        else
+            recv=0;
+        if(buck_rem!=0)
+        {
+            if(buck_rem<rate)
+            {
+                sent=buck_rem;
+                buck_rem=0;
+            }
+            else
+            {
+                sent=rate;
+                buck_rem=buck_rem-rate;
+            }
+        }
+        else
+            sent=0;
+        if(recv==-1)
+            System.out.println(i+"\t\t"+a[i]+"\t dropped\t"+sent+"\t"+buck_rem);
+        else
+            System.out.println(i+"\t\t"+a[i]+"\t\t"+recv+"\t"+sent+"\t"+buck_rem);
+    }
+}
